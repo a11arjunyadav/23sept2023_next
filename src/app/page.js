57683db3d4a281e8a3d95b2740c4969cc8a63b67
a11arjunyadav <yadavarjun0312@gmail.com>
2023-09-23 15:41:00 +0530

    c1

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+//1. Import area
+
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+import Main from "@/Components/Main";
+
+
+// Imort DefaultImport from 'some library/location';
+
+
+//2. Defintoin area
+ function Home() {
+  return (
+    <>
+      <main>
+        Home page 
+        
+      </main>
+    </>
+  )
+}
+//3. Export area
+//3.1 Default Ezport area
+ export default Home;
